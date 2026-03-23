741a2c43a4de50a4bbc9af9e27cff8e062c0833b
Jerry <jerry@heavydealers.com>
2026-03-23 12:44:05 +0000

    feat: Add ScrollToTop component to automatically scroll to the top of the page on route changes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import MainRoutes from "./components/routes/main-routes";
+import ScrollToTop from "./components/common/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <MainRoutes />
     </Router>
   );
